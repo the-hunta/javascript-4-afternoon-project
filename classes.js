448b42c52ed4c1767105936133a94d8b4c68b704
HS <HS@a218s-MacBook-Pro.local>
2019-05-17 16:25:02 -0600

    doneski

--- classes.js
+++ classes.js
@@ -157,6 +157,29 @@ class ProgressiveManager extends Manager{
         - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
-//Code Here
+
+class Machine{
+  constructor(){
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
+
+  makeWidgets(number) {
+    this.widgets_made_count += number;
+    this.wear_and_tear_count += number/50;
+  } 
+
+  fixMachine(){
+    this.needs_reboot = true
+  }
+
+  reboot(){
+    return function(){
+      this.wear_and_tear_count -= 10;
+      this.needs_reboot = false;
+    }
+  }
+}
 
 
