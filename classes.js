508b2ffd89f6787da84ea866530ee85cdcb779ee
HS <HS@a218s-MacBook-Pro.local>
2019-05-17 15:52:56 -0600

    makes sense

--- classes.js
+++ classes.js
@@ -29,7 +29,18 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+    makeWidget(){
+    return this.first_name + ' ' + this.last_name + " Widget"
+  }
+}
+
 
 
 ////////// PROBLEM 2 //////////
@@ -47,7 +58,20 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age) 
+    this.reports = []
+  }  
+  hire(employee){
+    this.reports.push(employee)
+  }
+
+  fire(index){
+    this.reports.splice(index,1)
+  }
+
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -71,7 +95,42 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager extends Manager{
+    constructor(first_name, last_name, email, age, title){
+      super(first_name, last_name, email, age)
+      this.title = 'Not a manager'
+      this.bonus = 0;
+    }
+    hire(employee){
+      this.reports.push(employee);
+      let numReports =  this.reports.length;
+      if(numReports === 0){
+        this.title = 'Not a Manager'
+      } else if (numReports <= 3 && numReports >= 1){
+        this.title = 'Barely Manager'}
+
+        else if (numReports <= 10 && numReports >= 4){
+        this.title = 'Mostly Manager'}
+
+        else if (numReports <= 50 && numReports >= 11){
+        this.title = 'Manager'}
+
+        else if (numReports <= 100 && numReports >= 51){
+        this.title = 'Manager Plus'}
+
+        else{
+        this.title = 'Bestest Manager'}
+
+    }
+  
+    fire(index){
+      this.reports.splice(index,1)
+      this.bonus += 100
+    } 
+
+    
+
+}
 
 
 
--- context.js
+++ context.js
@@ -19,14 +19,21 @@
 */
   
 
-//Code Here
+let user = {
+  username: 'huntsmith11',
+  email: 'huntsmith11@gmail.com',
+  getUsername: function(){
+    return this.username
+  }
+}
 
 
 
 ////////// PROBLEM 2 //////////
 
 /*
- Below we have the class Animal.  The eat method is using the "this" keyword.  Use the "new" keyword to assign context to "this", and save the instance to a variable named animal1.  You can pass anything you want in for name, species and food.
+ Below we have the class Animal.  The eat method is using the "this" keyword.  Use the "new" keyword to assign context to "this", and save the instance to a variable named animal1. 
+  You can pass anything you want in for name, species and food.
 */
 
 class Animal {
@@ -41,8 +48,7 @@ class Animal {
   }
 }
 
-//Code Here
-
+let animal1 = new Animal('big pappa', 'lion', 'meat')
 
 
 ////////// PROBLEM 3 //////////
@@ -61,7 +67,7 @@ let who = {
   location: 'Belize'
 }
 
-//Code Here
+let whoSaysHi = sayHi.bind(who)
 
 
 
@@ -72,11 +78,12 @@ let who = {
 */
 
 function whatIsThis() {
+console.log(this)
   return this
 }
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 = window
 
 let product = {
   name: 'snake plant',
@@ -87,7 +94,7 @@ let product = {
 let func = whatIsThis.bind(product)
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+let context2 = product
 
 let vacation = {
   location: 'Hawaii',
@@ -98,7 +105,7 @@ let vacation = {
 }
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
+let context3 = vacation.whatIsThis()
 
 
 class Family {
@@ -116,4 +123,4 @@ class Family {
 let family1 = new Family(2, 4, 1)
 
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-//let context4 = ???
+let context4 = family1
